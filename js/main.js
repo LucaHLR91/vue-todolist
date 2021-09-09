d42bca134688f9ee74ec7bb1c048c754bd4fc6c7
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-09 14:54:15 +0200

    Add toDo

--- js/main.js
+++ js/main.js
@@ -4,12 +4,27 @@
 // - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
 
 Vue.config.devtools = true;
-
 const app = new Vue(
     {
         el: '#root',
         data: {
-            
+           newToDo: '', 
+           toDoList: [
+               'Fare la spesa ore 8:00',
+               'Corso Boolean ore 9:30',
+               'Allenamenti ore 18:00',
+               'Palestra ore 20:00'
+           ]
+        },
+        methods: {
+            addToDo() {
+                if (this.newToDo != '') {
+                    this.toDoList.push(this.newToDo);
+                    this.newToDo = '';
+                } else {
+                    alert('Inserisci un compito')
+                }
+            }
         }
     }
 ) 
